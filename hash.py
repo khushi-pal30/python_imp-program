29a991a0d47922f30cc417ffa6934520de56fdef
Khushi pal <palkhushi163@gmail.com>
2025-08-02 22:52:42 +0530

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,8 +1,13 @@
-def hash_function(value):
-  sum_of_chars = 0
-  for char in value:
-    sum_of_chars += ord(char)
+# def hash_function(value):
+#   sum_of_chars = 0
+#   for char in value:
+#     sum_of_chars += ord(char)
 
-  return sum_of_chars % 10
+#   return sum_of_chars % 10
 
-print("'Bob' has hash code:", hash_function('Bob'))
+# print("'Bob' has hash code:", hash_function('Bob'))
+def contains(name):
+  index = hash_function(name)
+  return my_list[index] == name
+
+print("'Pete' is in the Hash Table:", contains('Pete'))
