a68adf7bc53c322c9721c35f9baac6c268ae750b
Khushi pal <palkhushi163@gmail.com>
2025-08-07 22:49:18 +0530

    Create datavisual.py

---
+++ datavisual.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4],[1,4,9,16]) #This is x-axis and y-axis value 
+plt.ylabel("Numbers") 
+plt.xlabel("Indeces")
+plt.title("Easy Ploting")
+plt.grid(True) # It defines the grid ploting
+plt.show() # It shows the result
